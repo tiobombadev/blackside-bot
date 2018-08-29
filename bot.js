8404fc91ec2f14563ce0280f079331c1953fe8c1
tiobombadev <42790268+tiobombadev@users.noreply.github.com>
2018-08-28 22:01:30 -0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,115 @@
+const Discord = require("discord.js"); //baixar a lib
+const client = new Discord.Client(); 
+const config = require("./config.json"); 
+
+
+client.on("ready", () => {
+  console.log(`Bot foi iniciado, com ${client.users.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores.`); 
+  client.user.setActivity(`Jogando | Criado por TioBomba`);
+// caso queira o bot trasmitindo use:
+
+   //client.user.setPresence({ game: { name: 't!ip', type: 1, url: 'tiobomba.tk'} });
+      //0 = Jogando
+      //1 = Transmitindo
+      //2 = Ouvindo
+      //3 = Assistindo
+      
+});
+
+client.on("guildCreate", guild => {
+  console.log(`O bot entrou nos servidor: ${guild.name} (id: ${guild.id}). População: ${guild.memberCount} membros!`);
+  client.user.setActivity(`Estou em ${client.guilds.size} servidores`);
+});
+
+client.on("guildDelete", guild => {
+  console.log(`O bot foi removido do servidor: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Estou em ${client.guilds.size} servidores`);
+});
+
+
+client.on("message", async message => {
+
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+    if(!message.content.startsWith(config.prefix)) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const comando = args.shift().toLowerCase();
+  
+  // coamdno ping
+  if(comando === "ping") {
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms. A Latencia da API é ${Math.round(client.ping)}ms`);
+  }
+  //comando falar
+  if(comando === "say") { 
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{});  
+    message.channel.send(sayMessage);
+  }
+//comando apagar
+  if(comando === "apagar") {
+    const deleteCount = parseInt(args[0], 10);
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Por favor, forneça um número entre 2 e 100 para o número de mensagens a serem excluídas");
+    
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Não foi possível deletar mensagens devido a: ${error}`));
+  }
+  // comando chutar 
+  if(comando === "kick") {
+    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+      return message.reply("Desculpe, você não tem permissão para usar isto!");
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("Por favor mencione um membro válido deste servidor");
+    if(!member.kickable) 
+      return message.reply("Eu não posso expulsar este usuário! Eles pode ter um cargo mais alto ou eu não tenho permissões de expulsar?");
+    
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "Nenhuma razão fornecida";
+    
+    await member.kick(reason)
+      .catch(error => message.reply(`Desculpe ${message.author} não consegui expulsar o membro devido o: ${error}`));
+    message.reply(`${member.user.tag} foi kickado por ${message.author.tag} Motivo: ${reason}`);
+
+  }
+  // comando ban
+  if(comando === "ban") {
+    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
+      return message.reply("Desculpe, você não tem permissão para usar isto!");
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Por favor mencione um membro válido deste servidor");
+    if(!member.bannable) 
+      return message.reply("Eu não posso banir este usuário! Eles pode ter um cargo mais alto ou eu não tenho permissões de banir?");
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "Nenhuma razão fornecida";
+    await member.ban(reason)
+      .catch(error => message.reply(`Desculpe ${message.author} não consegui banir o membro devido o : ${error}`));
+    message.reply(`${member.user.tag} foi banido por ${message.author.tag} Motivo: ${reason}`);
+  }
+  // comando ip 
+  if(comando === "ip") {
+    const m = await message.channel.send(`Nao temos ip fixo :/ , nosso desenvolvedor esta acabando o plugins ainda`);
+  }
+  // comando download
+  if(comando === "download") {
+    const m = await message.channel.send(`Meu criador tambem faz plugins para minecraft sabia ? baixe ou compre no site dele > Tiobomba.tk`);
+  }
+  // comando Convite
+  if(comando === "convite") {
+    const m = await message.channel.send(`Aqui esta o convite do servidor https://discord.me/RedeBlackSide`)
+  }
+  // comando teste
+  if(comando === "teste") {
+    const m = await message.channel.send("se usou este comando e por q vc e viado")
+  }
+  // comando criador
+  if(comando === "criador") {
+  const m = await message.channel.send("Meu criador foi o TioBomba#8805 , se quiser um bot chame ele :D")
+  }
+});
+
+client.login(config.token);
